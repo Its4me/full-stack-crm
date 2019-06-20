afed67a437271b60ea0994e57f7f1287493282c6
Yaroslav <Sukhorukovyar@gmail.com>
2019-06-20 15:05:09 +0300

    heroku deploy v1.0.7

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,9 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./keys.prod')
-} else {
-  module.exports = require('./keys.dev')
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = require('./keys.prod')
+// } else {
+//   module.exports = require('./keys.dev')
+// }
+module.exports = {
+  mongoURI: 'mongodb://yaroslav:12345678z@ds239157.mlab.com:39157/crm-fullstack',
+  jwt: 'dev-jwt'
 }
